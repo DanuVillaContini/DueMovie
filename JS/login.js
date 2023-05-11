905854d8b956a5af530d43a3bb8ab810406155ab
Danu Villa <126210362+DanuVillaContini@users.noreply.github.com>
2023-05-11 00:16:44 -0300

    Agregar muchos cambios

--- JS/login.js
+++ JS/login.js
@@ -1,36 +1,3 @@
-/* Página o modal de Login
-El diseño de esta página es opcional.
-El formulario de login debe solicitar un nombre de usuario o email y contraseña. Estos campos deben estar correctamente validados.  
-Incorporar un link para recuperar la contraseña en caso de no recordarla, al presionar este link mostrar los pasos a seguir para poder recuperar la contraseña.
-Si me logueo como el usuario administrador, me debe redirigir a la web de administración, si el usuario y contraseña ingresados no existe, debo informar con un modal o alert. */
-
-
-// document.getElementById('login-form').addEventListener('submit', function(event) {
-//     event.preventDefault();
-    
-//     // Obtener los valores de los campos de entrada
-//     let username = document.getElementById('username').value;
-//     let password = document.getElementById('password').value;
-  
-//     // Validar los campos de entrada
-//     if (username.trim() === '') {
-//       alert('Por favor ingresa tu nombre de usuario o correo electrónico.');
-//       return false;
-//     }
-  
-//     if (password.trim() === '') {
-//       alert('Por favor ingresa tu contraseña.');
-//       return false;
-//     }
-  
-//     // Simular un inicio de sesión exitoso
-//     if (username === 'admin' && password === 'admin') {
-//       window.location.href = 'admin.html';
-//     } else {
-//       alert('Usuario y/o contraseña incorrectos.');
-//     }
-//   });  
-
 // ---- VARIABLES PARA OBTENER LO INGRESADO EN LOS INPUT LOGIN -----
 const loginForm = document.querySelector('#login-form');
 const submitButton = document.querySelector('#submit-button');
@@ -75,7 +42,7 @@ function login(event) {
 
     //---- VALIDADO LOS DATO = REDIRIGIR A PAGE ADMIN (si no, modal alert)------
     if (user) {
-        window.location.href = 'welcome.html';
+        window.location.href = 'admin.html';
     } else {
         mostrarModal('Email o password incorrectos.')
         
---
+++ JS/register.js
@@ -0,0 +1,74 @@
+class NewUser {
+    email;
+    pass;
+    firstName;
+    lastName;
+
+    constructor(nameEmail, pass, firstName, lastName) {
+        this.email = nameEmail
+        this.pass = pass
+        this.firstName = firstName
+        this.lastName = lastName
+    }
+}
+
+
+//-------------------VARIABLES DE LOS MODALS------------------------------
+let modalMensajeExito = document.getElementById("modal-msg");
+let buttonCancelar = document.getElementsByClassName("close")[0];
+let modalMsgAdvertencia = document.getElementById("modal-msg-advertencia")
+
+//Funcion que abre el modal cuando se registra exitosamente
+function mostrarModal() {
+    modalMensajeExito.style.display = "block";
+}
+//Funcion button cancelar
+function cerrarModal() {
+    modalMensajeExito.style.display = "none";
+}
+
+//Funcion modal con mensaje de advertencia antes de click btt registrarse
+function msjAdvertencia() {
+    modalMsgAdvertencia.style.display = "block";
+}
+//Funcion button cancelar
+function cerrarModalAdv() {
+    modalMsgAdvertencia.style.display = "none";
+}
+
+
+//---------- FUNCION REGISTRAR NUEVO USUARIO -----------------------------------
+function guardarRegistro() {
+    //-----------VARIABLES-----------
+    let emailInput = document.getElementById('email-input').value;
+    let passInput = document.getElementById('password-input').value;
+    let firstNameInput = document.getElementById('firstName-input').value;
+    let lastNameInput = document.getElementById('lastName-input').value;
+
+    // Validar campos requeridos
+    if (!emailInput || !passInput || !firstNameInput || !lastNameInput) {
+        //Funcion modal de advertencia que no lleno todos los campos necesarios para poder registrarse
+        msjAdvertencia()
+        return;
+    }
+
+    //-----------CREO NUEVO OBJETO USER-----------
+    let userNew = new NewUser(emailInput, passInput, firstNameInput, lastNameInput);
+
+    //-----------GUARDAR OBJETO USER EN EL LOCAL STORAGE-----------
+    let usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
+    usuarios.push(userNew);
+    localStorage.setItem('usuarios', JSON.stringify(usuarios));
+
+    //-----------BORRAR LOS VALORES DE LOS INPUTS-----------
+    document.getElementById('email-input').value = '';
+    document.getElementById('password-input').value = '';
+    document.getElementById('firstName-input').value = '';
+    document.getElementById('lastName-input').value = '';
+
+    //----------MENSAJE MODAL DE REGISTRO EXITOSO Y PROXIMOS PASOS-----------
+    mostrarModal()
+
+}
+
+//--------------------------------------------------------
